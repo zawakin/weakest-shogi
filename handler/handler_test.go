b0053e54956997080388bbb4aef807f418a2f8d4
zawawahoge <zawawabis@gmail.com>
2020-05-24 21:32:14 +0900

    add test of Go and Quit in handler

--- handler/handler_test.go
+++ handler/handler_test.go
@@ -3,10 +3,20 @@ package handler
 import (
 	"bytes"
 	"context"
+	"errors"
+	"os"
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
 )
+
+func runSingleCommand(cmd string) string {
+	stdout := new(bytes.Buffer)
+	h := NewUSIHandler(stdout)
+	h.HandleCommand(context.Background(), cmd)
+
+	return string(stdout.Bytes())
+}
 
 func Test_USIHandler_HandleCommand_FirstUSI(t *testing.T) {
 	cmd := "usi"
@@ -14,13 +24,7 @@ func Test_USIHandler_HandleCommand_FirstUSI(t *testing.T) {
 id author zawawahoge
 usiok
 `
-
-	stdout := new(bytes.Buffer)
-	h := NewUSIHandler(stdout)
-	h.HandleCommand(context.Background(), cmd)
-
-	got := string(stdout.Bytes())
-
+	got := runSingleCommand(cmd)
 	if got != want {
 		t.Errorf("h.HandleCommand(ctx,%s); got=%s; want=%s\ncmp.Diff(got,want)=%s", cmd, got, want, cmp.Diff(got, want))
 	}
@@ -30,13 +34,53 @@ func Test_USIHandler_HandleCommand_IsReady(t *testing.T) {
 	cmd := "isready"
 	want := "readyok\n"
 
-	stdout := new(bytes.Buffer)
-	h := NewUSIHandler(stdout)
-	h.HandleCommand(context.Background(), cmd)
-
-	got := string(stdout.Bytes())
-
+	got := runSingleCommand(cmd)
 	if got != want {
 		t.Errorf("h.HandleCommand(ctx,%s); got=%s; want=%s\ncmp.Diff(got,want)=%s", cmd, got, want, cmp.Diff(got, want))
+	}
+}
+
+func Test_USIHandler_HandleCommand_NewGame(t *testing.T) {
+	cmd := "newgame"
+	want := ""
+
+	got := runSingleCommand(cmd)
+	if got != want {
+		t.Errorf("h.HandleCommand(ctx,%s); got=%s; want=%s\ncmp.Diff(got,want)=%s", cmd, got, want, cmp.Diff(got, want))
+	}
+}
+
+func Test_USIHandler_HandleCommand_Go(t *testing.T) {
+	cmd := "go"
+	want := "bestmove resign\n"
+
+	got := runSingleCommand(cmd)
+	if got != want {
+		t.Errorf("h.HandleCommand(ctx,%s); got=%s; want=%s\ncmp.Diff(got,want)=%s", cmd, got, want, cmp.Diff(got, want))
+	}
+}
+
+func Test_USIHandler_HandleCommand_Quit(t *testing.T) {
+	cmd := "quit"
+	want := false
+
+	h := NewUSIHandler(os.Stdout)
+	got, err := h.HandleCommand(context.Background(), cmd)
+	if err != nil {
+		t.Fatalf("h.HandleCommand(ctx,%s)=_, %#v; want nil", cmd, err)
+	}
+	if got != want {
+		t.Errorf("h.HandleCommand(ctx,%s)=%#v, nil; want %v", cmd, got, want)
+	}
+}
+
+func Test_USIHandler_HandleCommand_Empty(t *testing.T) {
+	cmd := ""
+	wantErr := errors.New("Command is empty")
+
+	h := NewUSIHandler(os.Stdout)
+	_, err := h.HandleCommand(context.Background(), cmd)
+	if err.Error() != wantErr.Error() {
+		t.Errorf("h.HandleCommand(ctx,%s)=_, %#v; want=%v", cmd, err, wantErr)
 	}
 }
